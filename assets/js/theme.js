1438e5b1b6547ebfd061f862d2652afba077e5dd
xevi2 <xevi2iglesias@gmail.com>
2026-05-20 19:14:25 +0200

    style: oscurecer iconos, botones e insignias (badges) que quedaban claros en el modo noche

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,6 +3,13 @@ function toggleTheme() {
     const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
     const newTheme = isDark ? 'light' : 'dark';
     document.documentElement.setAttribute('data-theme', newTheme);
+    
+    if (newTheme === 'dark') {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark');
+    }
+    
     localStorage.setItem('theme', newTheme);
     
     // Update button icon if exists
@@ -17,5 +24,6 @@ function toggleTheme() {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
         document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.classList.add('dark');
     }
 })();
